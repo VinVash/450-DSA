d539afd14e7c7632856bb8f6c71b32de0765ca91
VinVash <2020kucp1046@iiitkota.ac.in>
2023-02-15 22:03:56 +0530

    update: Morgan stanley OA question

--- MorganStanleyOA/1. n-lottery-tickets/main.cpp
+++ MorganStanleyOA/1. n-lottery-tickets/main.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+    Steps to select winner:
+
+    1. Lottery tickets are numbered 1 to n.
+    2. Delete first ticket, and then every alternative ticket till it reaches end.
+    3. Start from right, delete first ticket, and then every alternative ticket till it reaches end (start of the array).
+    4. Repeat steps 2. and 3. till only single ticket remains.
+    5. That ticket is the winner, return that ticket number.
+
+
+    Eg: There are 21 tickets:
+    1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+    2 4 6 8 10 12 14 16 18 20
+    2 6 10 14 20
+    6 14
+    6
+
+*/
+
 int main() {
 
     #ifndef ONLINE_JUDGE
