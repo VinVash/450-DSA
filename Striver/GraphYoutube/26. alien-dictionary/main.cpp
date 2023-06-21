32e768122f57b40ab6b39586ad297b435f8990ef
VinVash <2020kucp1046@iiitkota.ac.in>
2023-06-21 22:47:07 +0530

    update: Graph revision

--- Striver/GraphYoutube/26. alien-dictionary/main.cpp
+++ Striver/GraphYoutube/26. alien-dictionary/main.cpp
@@ -6,7 +6,65 @@ using namespace std;
 #define vvll vector<vector<long long>>
 typedef long long ll;
 
+// When the order will not be possible:
+// 1.) s1 = abcd and s2 = abc, the shorter string will always come before the largeer string.
+// 2.) if there is a cyclic dependency, not possible. Dictionary is wrong.
 
+// Time complexity: 
+// Space complexity: 
+
+vector<int> topoSort(vector<int> adj[], int V) {
+	int indegree[V] = {0};
+	for(int i = 0; i < V; i++) {
+		for(auto it: adj[i])
+			indegree[it]++;
+	}
+
+	queue<int> q;
+	for(int i = 0; i < V; i++) {
+		if(indegree[i] == 0)
+			q.push(i);
+	}
+
+	vector<int> topo;
+	while(!q.empty()) {
+		int node = q.front();
+		q.pop();
+
+		topo.push_back(node);
+
+		for(auto it: adj[node]) {
+			indegree[it]--;
+			if(indegree[it] == 0)
+				q.push(it);
+		}
+	}
+
+	return topo;
+}
+
+string findOrder(string dict[], int N, int K) {
+	vector<int> adj[K];
+	for(int i = 0; i < N-1; i++) {
+		string s1 = dict[i], s2 = dict[i+1];
+		int len = min(s1.size(), s2.size()); 
+		for(int ptr = 0; ptr < len; ptr++) {
+			if(s1[ptr] != s2[ptr]) {
+				adj[s1[ptr] - 'a'].push_back(s2[ptr] - 'a'); 
+				break; 
+			}
+		}
+	}
+
+	vector<int> topo = topoSort(adj, K);
+	string ans = "";
+	for(auto it: topo) {
+		ans = ans + char(it + 'a');
+	}
+
+	return ans;
+
+}
 
 
 int main() {
---
+++ Striver/GraphYoutube/27.shortest-path-in-directed-acyclic-graph/main.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+typedef long long ll;
+
+void dfs(int node, vector<pair<int, int>> adj[], int vis[], stack<int>& stk) {
+    vis[node] = 1;
+    for(auto it: adj[node]) {
+        int v = it.first;
+        if(!vis[v]) {
+            dfs(v, adj, vis, stk);
+        }
+    }
+    
+    
+    stk.push(node);
+}
+
+ vector<int> shortestPath(int N,int M, vector<vector<int>>& edges) {
+    vector<pair<int, int>> adj[M];
+    for(int i = 0; i < N; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        int wt = edges[i][2];
+        adj[u].push_back({ v, wt });
+    }
+    
+    // find the topo sort
+    // O(N+M)
+    int vis[N] = {0};
+    stack<int> stk;
+    for(int i = 0; i < N ;i++) {
+        if(!vis[i])
+            dfs(i, adj, vis, stk);
+    }
+    
+    // step 2: do the distance thing.
+    vector<int> dist(N);
+    for(int i = 0; i < N; i++)
+        dist[i] = 1e9;
+        
+    dist[0] = 0; // dist of src node is 0.
+    
+    // O(N+M)
+    while(!stk.empty()) {
+        int node = stk.top();
+        stk.pop();
+        
+        for(auto it: adj[node]) {
+            int v = it.first;
+            int wt = it.second;
+            
+            if(dist[node] + wt < dist[v])
+                dist[v] = dist[node] + wt;
+        }
+    }
+    
+    return dist;
+}
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	#endif
+
+
+	return 0;
+}
