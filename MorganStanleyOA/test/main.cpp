ad27de8249f5aeb8bc0ca1169dd810f5d42a84cc
VinVash <2020kucp1046@iiitkota.ac.in>
2023-06-23 15:58:47 +0530

    update: organised the repo

---
+++ MorganStanleyOA/test/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+typedef long long ll;
+
+
+int main() {
+
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	#endif
+
+
+	return 0;
+}
